00e73bb6aed68d551c9c1a030bd73a22f6942dfe
Partha Aji <paji@redhat.com>
2019-05-08 11:28:25 -0400

    Updates

---
+++ precache-manifest.22d0d65f2271983ddfac1474d6d68342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09273b533b994165094cb96c034c08c7",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "3ca20df7d6d907cafebd",
+    "url": "/redux-todo/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "1a200c1133a345daab8c",
+    "url": "/redux-todo/static/js/2.55463bfb.chunk.js"
+  },
+  {
+    "revision": "3ca20df7d6d907cafebd",
+    "url": "/redux-todo/static/js/main.536bb0fb.chunk.js"
+  },
+  {
+    "revision": "4a2d5658e1b8a4be0a5f",
+    "url": "/redux-todo/static/js/runtime~main.134d15df.js"
+  }
+]);
